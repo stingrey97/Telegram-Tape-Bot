19b6d044a2e6007ad97ecea21253c2d25efb1cee
Vincent Terzenbach <vincent@terzenbach.com>
2024-11-03 16:50:33 +0100

    Only logs sent text, not whole message object.
    Took 10 minutes

--- src/main/java/de/stingrey97/telegramtapebot/service/ChatService.java
+++ src/main/java/de/stingrey97/telegramtapebot/service/ChatService.java
@@ -31,7 +31,7 @@ public class ChatService {
 
         try {
             telegramClient.execute(message);
-            logger.info("Message sent to ChatId '{}': {}", chatId, message);
+            logger.info("Message sent to ChatId '{}': {}", chatId, text);
             Thread.sleep(900);
         } catch (Exception e) {
             logger.error("Unknown error in reply method! Text was: '{}' by chatID: '{}'", text, chatId, e);
